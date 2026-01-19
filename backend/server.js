b132efe33802fd2d323b8330a2bd6f978d6e1654
YogeshSabbani1213 <yogeshsabbani67@gmail.com>
2026-01-19 23:02:33 +0530

    Added server.js

--- backend/server.js
+++ backend/server.js
@@ -7,3 +7,7 @@ import connectDB from "./src/config/db.js";
 connectDB();
 const PORT = process.env.PORT
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
+app.get('/',(req,res)=>{
+    res.send('server is running');
+})
