96d7304bcb65496183034b5c80c00e543edb4b2c
YogeshSabbani1213 <yogeshsabbani67@gmail.com>
2026-01-19 20:47:41 +0530

    backend: add user model

---
+++ backend/src/models/UserModel.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    avatar:{type:String},
+    channels: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Channel' }]
+}, { timestamps: true });
+
+const userModel = mongoose.model("User", userSchema);
+export default userModel
