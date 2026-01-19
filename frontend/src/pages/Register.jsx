4db2eccbe7fb356593c2b9a428dc12fb43fd778b
YogeshSabbani1213 <yogeshsabbani67@gmail.com>
2026-01-19 21:19:16 +0530

    frontend: add register page

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,168 @@
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+
+function Register({ setAuthView }) {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleRegister(e) {
+    e.preventDefault();
+    try{
+      const response = await fetch("http://localhost:8080/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          email: email,
+          password: password,
+        }),
+      });
+      const data =await response.json()
+  
+      if(!response.ok){
+        toast.error(data.message || 'Registration Failed');
+        return;
+      }
+        console.log(data)
+        toast.success('Registration Success',{
+        style: { fontSize:"20px" ,borderRadius: '10px', background: '#333', color: '#fff' }
+      });
+        setUsername("");
+        setEmail("")
+        setPassword("");
+        setAuthView(null);
+    }
+    catch(error){
+      console.error(error);
+      toast.error('something went wrong');
+    }
+
+  }
+  return (
+    <div>
+      <section className="bg-gray-50 dark:bg-gray-900">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="lg:text-3xl text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Create an account
+              </h1>
+              <form
+                onSubmit={handleRegister}
+                className="space-y-4 md:space-y-6"
+                action="#"
+              >
+                {/*username*/}
+                <div>
+                  <label
+                    for="UserName"
+                    className="lg:text-xl block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    User Name
+                  </label>
+                  <input
+                    type="username"
+                    name="username"
+                    id="username"
+                    placeholder="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    className="lg:text-xl bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required=""
+                  />
+                </div>
+
+                {/*email*/}
+                <div>
+                  <label
+                    for="email"
+                    className=" lg:text-xl block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Your email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="lg:text-xl bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="name@company.com"
+                    required=""
+                  />
+                </div>
+
+                {/*password*/}
+                <div>
+                  <label
+                    for="password"
+                    className="lg:text-xl block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="••••••••"
+                    className=" lg:text-xl bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required=""
+                  />
+                </div>
+
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                    <input
+                      id="terms"
+                      aria-describedby="terms"
+                      type="checkbox"
+                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                      required=""
+                    />
+                  </div>
+                  <div className="ml-3 text-sm">
+                    <label
+                      for="terms"
+                      className=" lg:text-xl font-light text-gray-500 dark:text-gray-300"
+                    >
+                      I accept the{" "}
+                      <a
+                        className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                        href="#"
+                      >
+                        Terms and Conditions
+                      </a>
+                    </label>
+                  </div>
+                </div>
+                <button
+                  type="submit"
+                  className="lg:text-xl w-full text-black bg-blue-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Create an account
+                </button>
+                <p className="lg:text-lg text-sm font-light text-gray-500 dark:text-gray-400">
+                  Already have an account?{" "}
+                  <button
+                    type="button"
+                    className="lg:text-lg font-medium text-blue-500"
+                    onClick={() => setAuthView("login")}
+                  >
+                    Login here
+                  </button>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Register;
